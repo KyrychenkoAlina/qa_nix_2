6c5a26ffefbabcb23c31deedbf0357635bbe9499
Alina Kyrychenko <aalinakir@gmail.com>
2021-08-14 12:45:34 +0300

    init commit

--- src/main/java/ua/com/alevel/Main.java
+++ src/main/java/ua/com/alevel/Main.java
@@ -1,5 +1,9 @@
+package ua.com.alevel;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        ua.com.alevel.HomeworkOne.main(new String[]{"sss", "hhh"});
+
     }
 }

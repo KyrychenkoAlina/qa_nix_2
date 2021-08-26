6a0de2ee1527a2723479b241b4bf139ff54b335a
Alina Kyrychenko <aalinakir@gmail.com>
2021-08-26 11:15:22 +0300

    hw 1

--- src/main/java/ua/com/alevel/Main.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
-        ua.com.alevel.HomeworkOne.main(new String[]{"sss", "hhh"});
-
-    }
-}
